8bb720ceed34adb933e9b6f38590b20a2f902652
julianmichael <julianjohnmichael@gmail.com>
2015-06-06 17:41:02 -0700

    added pure untyped lambda calc and tests

---
+++ src/main/scala/loco/PureLambda.scala
@@ -0,0 +1,120 @@
+package loco
+
+sealed trait PureLambda {
+  import PureLambda._
+  override def toString: String = this match {
+    case Var(x) => x
+    case Lam(p, t) => s"(\\$p. $t)"
+    case App(t1, t2) => s"($t1 $t2)"
+  }
+}
+object PureLambda {
+  // for notational convenience
+  type Exp = PureLambda
+
+  case class Var(name: String) extends Exp
+  case class Lam(param: String, t: Exp) extends Exp
+  case class App(t1: Exp, t2: Exp) extends Exp
+
+  def isValue(t: Exp): Boolean = t match {
+    case App(Var(_), _) => true
+    case App(_, _) => false
+    case _ => true
+  }
+
+  def girth(t: Exp): Int = t match {
+    case Var(_) => 1
+    case Lam(_, t1) => 1 + girth(t1)
+    case App(t1, t2) => 1 + girth(t1) + girth(t2)
+  }
+
+  def size(t: Exp): Int = t match {
+    case Var(_) => 1
+    case Lam(_, t1) => 1 + size(t1)
+    case App(t1, t2) => 1 + math.max(girth(t1), girth(t2))
+  }
+
+  def intsFrom(i: Int): Stream[Int] = i #:: intsFrom(i+1)
+  val nats = intsFrom(0).map(_.toString)
+  // stdlib bug; should just use filterNot when they make lazy again
+  def freshVar(prohib: Set[String]) = nats.filter(!prohib(_)).head
+
+  def freeVars(t: Exp): Set[String] = t match {
+    case Var(name) => Set(name)
+    case Lam(p, t) => freeVars(t) - p
+    case App(t1, t2) => freeVars(t1) ++ freeVars(t2)
+  }
+
+  def isClosed(t: Exp): Boolean = freeVars(t).isEmpty
+
+  def alpha(prohib: Set[String], lam: Lam): Lam = lam match {
+    case Lam(param, t) if(prohib(param)) => {
+      val newVar = freshVar(prohib)
+      Lam(newVar, substitute(t, param, Var(newVar)))
+    }
+    case x => x
+  }
+
+  // substitute `sub` for `name` in `target`
+  def substitute(sub: Exp, name: String, target: Exp): Exp = target match {
+    case Var(y) if y == name => sub
+    case v@Var(_) => v
+    case l@Lam(_, _) => {
+      val Lam(newP, newT) = alpha(freeVars(sub) ++ freeVars(l), l)
+      val subbedTerm = substitute(sub, name, newT)
+      Lam(newP, subbedTerm)
+    }
+    case App(t1, t2) => {
+      val newT1 = substitute(sub, name, t1)
+      val newT2 = substitute(sub, name, t2)
+      App(newT1, newT2)
+    }
+  }
+
+  def step(t: Exp): Exp = t match {
+    case v if isValue(v) => v
+    case App(t1, t2) if !isValue(t1) => App(step(t1), t2)
+    case App(v1, t2) if !isValue(t2) => App(v1, step(t2))
+    case App(Lam(p, t), v2) => substitute(v2, p, t)
+    // otherwise we're stuck (free variable) or looping
+  }
+
+  def smallStepEval(t: Exp): Exp = {
+    var term = t
+    var nextTerm = step(t)
+    while(term != nextTerm) {
+      term = nextTerm
+      nextTerm = step(nextTerm)
+    }
+    term
+  }
+
+  def bigStepEval(t: Exp): Exp = t match {
+    case v if isValue(v) => v
+    case App(t1, t2) => {
+      val v1 = bigStepEval(t1)
+      val v2 = bigStepEval(t2)
+      (v1: @unchecked) match {
+        case Lam(p, t) => {
+          println(s"$v1 $v2")
+          bigStepEval(substitute(t, p, v2))
+        }
+        // otherwise we're stuck (free variable) or looping
+      }
+    }
+  }
+
+  def superEval(t: Exp): Exp = t match {
+    case v@Var(_) => v
+    case l@Lam(p, t1) => Lam(p, superEval(t1))
+    case App(t1, t2) => {
+      val v1 = superEval(t1)
+      val v2 = superEval(t2)
+      (v1: @unchecked) match {
+        case Lam(p, tt) => superEval(substitute(tt, p, v2))
+        case _ => App(v1, v2)
+      }
+    }
+  }
+
+}
---
+++ src/test/scala/loco/PureLambdaTest.scala
@@ -0,0 +1,46 @@
+package loco
+
+import org.scalatest.FunSuite
+
+class TestSuite extends FunSuite {
+  import PureLambda._
+  import ChurchEncodings._
+
+  test("isValue") {
+    assert(isValue(id) === true)
+    assert(isValue(tru) === true)
+    assert(isValue(App(id, id)) === false)
+  }
+
+  test("girth") {
+    assert(girth(id) === 2)
+    assert(girth(mock) === 4)
+  }
+
+  test("size") {
+    assert(size(id) === 2)
+    assert(size(mock) === 3)
+  }
+
+  test("freeVars") {
+    assert(freeVars(id) === Set.empty[String])
+    assert(freeVars(Var("x")) === Set("x"))
+  }
+
+  test("isClosed") {
+    assert(isClosed(id) === true)
+  }
+
+  //TODO alpha subst and other tests
+
+  test("step") {
+    assert(step(App(id, App(id, id))) === App(id, id))
+    assert(step(App(id, id)) === id)
+  }
+
+  test("smallStepEval") {
+    assert(smallStepEval(App(id, App(id, id))) === id)
+  }
+
+  // TODO big step eval and other tests
+}
